8081eeb19153a80e9c4c8c53fbcd972ad810462e
connorvm <connormannion24@yahoo.com>
2022-10-13 22:29:37 -0400

    commit tree 20c63fe0b09546e73199e4951ea5156ce78afaff

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+     moveDodgerLeft();
+    }
+  });
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+  }
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px","");
+    const right = parseInt(10, rightNumbers);
+
+    if(right > 400) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
